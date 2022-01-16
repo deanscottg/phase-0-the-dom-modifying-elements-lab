ac391e8cfcccfdae295605d8207f6c2222807351
Dean <deanscottg@gmail.com>
2022-01-15 22:08:07 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const myElement = document.getElementById("main")
+myElement.remove("main");
+
+const newHeader = document.createElement("h1");
+    newHeader.textContent ="victory";
+    newHeader.id= "victory";
+    newHeader.innerHTML= "is the champion";
